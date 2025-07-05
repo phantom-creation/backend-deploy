999cfcbbbbf52152ed6842bd9ce04a2b522d0b74
phantom creation <phantomcreation.dev@gmail.com>
2025-07-05 12:32:36 +0530

    change cookie setting

--- src/user/userController.js
+++ src/user/userController.js
@@ -9,8 +9,8 @@ const sendToken = (user, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    secure: true,
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
